a3bbff32b5506c2ac42d571f7fe3ac07131488cb
yoshi <1789197314@qq.com>
2023-02-03 23:21:55 +0800

    feat: 新增炫彩模式

--- components/Progress/Progress.tsx
+++ components/Progress/Progress.tsx
@@ -1,9 +1,10 @@
 // author: yoshino
 import { getPrefix } from '../_utils/prefix'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import './style.less'
 import useCounter from 'ahooks/lib/useCounter'
 import { classNames } from '../_utils/classNames'
+import { useInterval, useUpdate } from 'ahooks'
 
 interface ProgressProps {
   showText?: boolean
@@ -16,12 +17,35 @@ interface ProgressProps {
 
 function Progress(props: ProgressProps): JSX.Element {
   const [percent, { set }] = useCounter(props.value, { min: 0, max: 100 })
+  const color = useRef<ProgressProps['color']>(props.color)
+  const update = useUpdate()
+  useInterval(
+    () => {
+      if (props.colorful === false) return
+      switch (color.current) {
+        case 'primary':
+          color.current = 'secondary'
+          break
+        case 'secondary':
+          color.current = 'tertiary'
+          break
+        case 'tertiary':
+          color.current = 'primary'
+          break
+        case undefined:
+          color.current = 'primary'
+      }
+      update()
+    },
+    1000,
+    { immediate: true }
+  )
   useEffect(() => {
     set(props.value)
   }, [props.value])
   const mainName = getPrefix('progress')
   return (
-    <div className={classNames(mainName, props)}>
+    <div className={classNames(mainName, props, { color: color.current })}>
       <div className={`${mainName}-bar-container`}>
         <div
           className={`${mainName}-width-bar`}
