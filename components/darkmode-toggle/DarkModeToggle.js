f978da9173c8ae083a54015bd2db8ce92c9bf5dd
Diana7mi <103913514+Diana7mi@users.noreply.github.com>
2023-02-15 22:00:39 +0800

    Create DarkModeToggle.js

---
+++ components/darkmode-toggle/DarkModeToggle.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './DarkModeToggle.css';
+
+const DarkModeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleToggle = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <div className={`dark-mode-toggle ${isDarkMode ? 'dark' : 'light'}`}>
+      <button onClick={handleToggle}>
+        {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      </button>
+    </div>
+  );
+};
+
+export default DarkModeToggle;
